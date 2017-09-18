d9f089feeb85fa4b943c783bf51e2368b8275c5d
Liusha <liusha@wizeline.com>
2017-09-17 19:20:39 -0700

    updated tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,11 @@
 # flake8: noqa
 from falcon import testing
 
+from config import Test
 from api.server import App
 
 
 class TestCase(testing.TestCase):
     def setUp(self):
         super(TestCase, self).setUp()
-        self.app = App()
+        self.app = App(Test)
